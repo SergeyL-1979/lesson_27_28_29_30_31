44568edcc1fc7fd1e747e047d86397acb64addf6
sergd <videomc1979@gmail.com>
2023-03-27 09:40:17 +0300

    HW_30 test

--- hunting/settings.py
+++ hunting/settings.py
@@ -11,17 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Для debug версии
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Для сервера версии
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hv*^idq!ep+_5%8v2*=w=g7m@ot1y&^yi__a_20tqbmr8hzti#'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
